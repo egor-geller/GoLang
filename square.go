41e093c3d778c625a323794605c99e71b44ce7a3
egor-geller <56612094+egor-geller@users.noreply.github.com>
2019-11-16 15:39:14 +0300

    update square.go
    1. modified func area
    2. modified func receivers to consistent

--- square.go
+++ square.go
@@ -11,18 +11,18 @@ type Square struct {
 	a     uint
 }
 
-func (e Square) End() (x1, y1 int){
-	x1 = e.start.x + int(e.a)
-	y1 = e.start.y + int(e.a)
+func (s Square) End() (x1, y1 int){
+	x1 = s.start.x + int(s.a)
+	y1 = s.start.y + int(s.a)
 	return x1, y1
 }
 
-func (p Square) Perimeter() (p1 int)  {
-	return int(p.a)*4
+func (s Square) Perimeter() (p1 int)  {
+	return int(s.a)*4
 }
 
 func (s Square) Area() (s1 int) {
-	return int(s.a)*2
+	return int(s.a)*int(s.a)
 }
 
 func main() {
