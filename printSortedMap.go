4517cf4448a8774882fba204c438b7c2d741b639
egor-geller <56612094+egor-geller@users.noreply.github.com>
2019-11-21 02:11:19 +0300

    update printSortedMap.go

--- printSortedMap.go
+++ printSortedMap.go
@@ -1,19 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func printSorted(x map[int]string) {
-	var l int = len(x)
-	e := make(map[int]string, l)
+	keys := make([]int, 0, len(x))
 	for k := range x {
-		if k > k+1 {
-			e[k+1] = e[k]
-		}
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Print(x[k], " ")
 	}
 }
 
 func main() {
 	a := map[int]string{3: "hh", 0: "tt", 1: "rr", 2: "aa", 500: "ss", 350: "ty"}
 	printSorted(a)
-	fmt.Println(a)
 }
