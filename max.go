4ff4619695c3a9949e1a0c391e90f4a9928cbe8c
egor-geller <56612094+egor-geller@users.noreply.github.com>
2019-11-24 15:38:16 +0300

    update1_2 max.go
    add RuneCountInString

--- max.go
+++ max.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"unicode/utf8"
 )
 
 func max(x []string) string {
-	var stringLen, maxCharLen, p int
+	var stringLen, p int
 	if len(x) <= 0 {
 		fmt.Print("Slice has no elements")
 		os.Exit(1)
 	}
 	stringLen = len(x)
-	maxCharLen = len(x[0])
+	maxCharLen := utf8.RuneCountInString(x[0])
 	for i := 1; i < stringLen; i++ {
 		if maxCharLen < len(x[i]) {
 			maxCharLen = len(x[i])
