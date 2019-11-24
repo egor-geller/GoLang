9087dc2363363a4eac3cd2e94bbf61e6ca7efdbf
egor-geller <56612094+egor-geller@users.noreply.github.com>
2019-11-24 15:06:51 +0300

    update average.go
    add if N == 0

--- average.go
+++ average.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func average(x []int) float64 {
 	var sum, N int
 	sum = 0
 	N = len(x)
+	if N == 0 {
+		fmt.Print("Slice has no elements")
+		os.Exit(1)
+	}
 	for i := 0; i < N; i++ {
 		sum += x[i]
 	}
@@ -14,5 +21,6 @@ func average(x []int) float64 {
 
 func main() {
 	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	//a := []int{}
 	fmt.Println(average(a))
 }
