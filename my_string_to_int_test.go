b53591957862d1125251086933f16f038fa9cdc6
egor-geller <56612094+egor-geller@users.noreply.github.com>
2019-11-29 01:23:50 +0300

    update my_string_to_int_test.go
    add go fmt

--- my_string_to_int_test.go
+++ my_string_to_int_test.go
@@ -9,13 +9,13 @@ func Test_First(t *testing.T) {
 	numErr := "rtt"
 	num := 1234
 
-	str1,err1 := myStringToInt(numStr)
-	if str1 != num || err1 != nil{
+	str1, err1 := myStringToInt(numStr)
+	if str1 != num || err1 != nil {
 		t.Errorf("Error")
 	}
 
 	str2, err2 := myStringToInt(numErr)
-	if str2 == num || err2 == nil{
+	if str2 == num || err2 == nil {
 		t.Errorf("Error")
 	}
 }
