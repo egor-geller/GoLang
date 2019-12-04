b2236c962c5443d9a49586d8fb16dabe4b95a7b7
egor-geller <56612094+egor-geller@users.noreply.github.com>
2019-12-05 01:42:48 +0300

    update my_string_to_int.go
    func return

--- my_string_to_int.go
+++ my_string_to_int.go
@@ -6,9 +6,5 @@ import (
 )
 
 func myStringToInt(s string) (int, error) {
-	str, err := strconv.Atoi(s)
-	if err != nil {
-		return 0, fmt.Errorf("wrong input")
-	}
-	return str, nil
+	return strconv.Atoi(s)
 }
