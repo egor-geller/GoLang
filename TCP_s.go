f277ff63d772340892e9c1a036b69af7fc7eb002
egor-geller <56612094+egor-geller@users.noreply.github.com>
2019-12-19 00:19:51 +0300

    update TCP_s.go
    error function

--- TCP_s.go
+++ TCP_s.go
@@ -10,10 +10,20 @@ import (
 
 func main() {
 	fmt.Println("Launching server...")
-	ln, _ := net.Listen("tcp", ":8081")
-	conn, _ := ln.Accept()
+	ln, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		fmt.Errorf("Connection problem")
+	}
+	conn, err1 := ln.Accept()
+	if err1 != nil {
+		fmt.Errorf("Connection problem")
+	}
+
 	for {
-		message, _ := bufio.NewReader(conn).ReadString('\n')
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Errorf("Connection problem")
+		}
 		num, err := strconv.Atoi(message)
 		if err == nil {
 			fmt.Printf("Multiply by 2: %d\n", num*2)
